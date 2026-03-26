bd22ccedf7284bf442e7fbfeff3afd58dbb95dd8
F3lipeMelo <127317123+F3lipeMelo@users.noreply.github.com>
2026-03-26 17:56:28 +0000

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query();
   response.status(200).send("Tudo funcionando");
 }
 
